d5a921050d8090a7a7ba1d7e801b985e82675836
ericyou1988 <eric_you1988@outlook.com>
2019-09-06 17:44:11 +0800

    update 20190906

---
+++ ipython_script_test.py
@@ -0,0 +1,12 @@
+# def f(x, y, z):
+#     return (x + y) / z
+#
+# a = 5
+# b = 6
+# c = 7.5
+#
+# result = f(a, b, c)
+
+import numpy as np
+import matplotlib.pyplot as plt
+plt.plot(np.random.randn(50).cumsum())
