abad6b564ad9730903e24f1b70aa318089fc20b0
ericyou1988 <eric_you1988@outlook.com>
2019-09-12 17:53:17 +0800

    update 20190912

--- ipython_script_test.py
+++ ipython_script_test.py
@@ -10,3 +10,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 plt.plot(np.random.randn(50).cumsum())
+
+import re
+states = ['   Alabama ', 'Georgia!', 'Georgia', 'georgia', 'FlOrIda',
+          'south   carolina##', 'West virginia?']
+
+def clean_strings(strings):
+    result = []
+    for value in strings:
+        value = value.strip()
+        print(value)
+        value = re.sub('[!#?]', '', value)
+        print(value)
+        value = value.title()
+        print(value)
+        result.append(value)
+    return result
+clean_strings(states)
